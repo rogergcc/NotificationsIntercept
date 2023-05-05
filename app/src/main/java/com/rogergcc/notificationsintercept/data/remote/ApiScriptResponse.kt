f53462e7c9edfae7e2a8ae7129378f24919618b8
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2023-05-04 19:11:52 -0500

    ApiScriptResponse.kt

---
+++ app/src/main/java/com/rogergcc/notificationsintercept/data/remote/ApiScriptResponse.kt
@@ -0,0 +1,11 @@
+package com.rogergcc.notificationsintercept.data.remote
+
+
+/**
+ * Created on mayo.
+ * year 2023 .
+ */
+data class ApiScriptResponse(
+    val data: String?,
+    val result: String?,
+)
